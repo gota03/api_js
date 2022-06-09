f3ac3e9566eb5f62022d44891c34b81d5b512222
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-06-09 20:50:32 -0300

    implementando visualizacoes da api no html

--- nativo/script.js
+++ nativo/script.js
@@ -1,12 +1,35 @@
+//let lista = document.querySelector("#lista")
+let tabela = document.querySelector("#tabela")
+
 async function carregarDados(){
     const url = "https://swapi.dev/api/people/?format=json"
     try {
         let resultado = await fetch(url)
         const dados = await resultado.json()
         for (elementos of dados.results) {
-            console.log(`Sou ${elementos.name} e nasci no ano ${elementos.birth_year}`)
+            // CRIANDO ELEMENTOS HTML DINAMICAMENTE
+            //const itemLista = document.createElement("li")
+            //itemLista.classList.add("list-group-item")
+            //itemLista.textContent = `Sou ${elementos.name}, tenho ${elementos.height} de altura sou do gênero ${elementos.gender} e tenho a cor do cabelo ${elementos.hair_color}`
+            //lista.appendChild(itemLista)
+            const linha = document.createElement("tr")
+
+            const tdNome = document.createElement("td")
+            tdNome.textContent = `Meu nome é ${elementos.name}`
+            linha.appendChild(tdNome)
+
+            const tdPeso = document.createElement("td")
+            tdPeso.textContent = `o meu peso é ${elementos.mass}`
+            linha.appendChild(tdPeso)
+
+            const tdOlho = document.createElement("td")
+            tdOlho.textContent = `e a cor do meu olho é ${elementos.eye_color}`
+            linha.appendChild(tdOlho)
+
+            tabela.appendChild(linha)
+            
         }
-        //console.log(dados.results)
+        console.log(dados.results)
     } catch (error) {
         //console.log("o seguinte erro ocorreu: ", error)
     }
